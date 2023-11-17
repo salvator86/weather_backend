6ff5c4a298701d35f62c5c60883a5d5532d8f8f6
Oleksandr <demkov.sasha42@gmail.com>
2023-11-17 15:01:13 +0200

    finished backend side

--- src/weather/weather.controller.ts
+++ src/weather/weather.controller.ts
@@ -1,17 +1,23 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { WeatherService } from './weather.service';
+import { weatherData, weatherResponse } from '../types/weather';
 
 @Controller('weather')
 export class WeatherController {
   constructor(private readonly weatherService: WeatherService) {}
   @Get(':city')
-  async getWeatherByCity(@Param('city') city: string) {
+  async getWeatherByCity(
+    @Param('city') city: string,
+  ): Promise<Promise<weatherResponse> | { error: string }> {
     try {
-      const weatherData = await this.weatherService.getWeatherForCity(city);
-      console.log(weatherData);
+      const weatherData: weatherData =
+        await this.weatherService.getWeatherForCity(city);
       return { weatherData };
     } catch (error) {
-      return { error: 'Unable to fetch weather data' };
+      return {
+        error:
+          'You requested a not existed city or there is no available attempts to request :(',
+      };
     }
   }
 }
--- src/weather/weather.service.ts
+++ src/weather/weather.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
+import { weatherData } from '../types/weather';
 
 @Injectable()
 export class WeatherService {
   private apiKey: string = 'eyhcuT13gjlGtPvrsz0XsymGLzAUIYV5';
+
   constructor(private httpService: HttpService) {}
+
   async getLocationKey(city: string): Promise<string> {
     const apiUrl: string = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${this.apiKey}&q=${city}`;
 
@@ -16,21 +19,35 @@ export class WeatherService {
     }
   }
 
-  async getWeatherForCity(city: string): Promise<any> {
+  async getWeatherForCity(city: string): Promise<weatherData> {
     try {
       const locationKey: string = await this.getLocationKey(city);
       const apiUrl: string = `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${this.apiKey}&details=true`;
       const response = await this.httpService.get(apiUrl).toPromise();
       const temperature = response.data[0]?.Temperature.Metric.Value;
-      const { WeatherText } = response.data[0];
-      // const localTime = new Date(LocalObservationDateTime).toLocaleString();
+      const { WeatherText, LocalObservationDateTime } = response.data[0];
+      const localTime: string = new Date(
+        LocalObservationDateTime,
+      ).toLocaleString();
+
       return {
+        city,
         temperature,
-        // localTime,
+        localTime,
         WeatherText,
       };
     } catch (error) {
-      throw new Error('Unable to fetch weather data');
+      throw new Error(
+        'You requested a not existed city or there is no available attempts to request :(',
+      );
     }
   }
 }
+
+// {
+//     "weatherData": {
+//         "temperature": 4,
+//         "localTime": "17.11.2023, 12:57:00",
+//         "WeatherText": "Cloudy"
+//     }
+// }
